00fd15dd94f76ecf42d13599a9a57a5da516e4cd
Sergey Juchenko <srjuchenko@Sergeys-MacBook-Pro-2.local>
2022-10-07 12:50:01 +0300

    added files and final solution

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # adviceGenerator
+
+## Frontend Mentor - Advice generator app solution
+
+This is a solution to the [Advice generator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/advice-generator-app-QdUG-13db). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Media query
+- JavaScript
+- [Advice Slip API](https://api.adviceslip.com)
+
+### Screenshots
---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const adviceNumberEl = document.querySelector(".advice-number");
+const adviceTextEl = document.querySelector(".advice-text");
+const diceBtnEl = document.querySelector(".advice-dice");
+
+diceBtnEl.addEventListener("click", function (e) {
+  fetch("https://api.adviceslip.com/advice")
+    .then((response) => response.json())
+    .then((data) => {
+      adviceNumberEl.textContent = data.slip.id;
+      adviceTextEl.textContent = `"${data.slip.advice}"`;
+    })
+    .catch(() => (adviceTextEl.textContent = "Can't connect to the API"));
+});
